87db90b1a154e4abc301f98ed79ed3a44191114c
jyash28 <jyash548@gmail.com>
2021-06-23 11:17:37 +0530

    'update'

---
+++ chapter5/liststring.py
@@ -0,0 +1,7 @@
+#string are immutable
+#list are mutable
+s = "string"
+#s.title() # title don't change the first letter to capital
+t = s.title()
+print(t)
+
---
+++ chapter5/pop.py
@@ -0,0 +1,3 @@
+fruits = ["word1","word2","word3","word4"]
+fruits.pop()# or fruits.pop(1)at particular position
+print(fruits)
