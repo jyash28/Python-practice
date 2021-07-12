ed4c524776fde3f663fd5424ed91b3357b892ab3
jyash28 <jyash548@gmail.com>
2021-07-12 12:07:35 +0530

    update

---
+++ os module/os.py
@@ -0,0 +1,7 @@
+import os
+#print(os.getcwd()) 
+#os.mkdir("movies") # to create folder
+#print(os.path.exists("movies"))
+#open("file.txt","a").closed() # to create file
+#os.mkdir(r"F:\stuff\movies") # to work in another folder
+print(os.listdir())
